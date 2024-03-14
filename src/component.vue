0b3ddfe511104f6aeac5f7a252605ace50b95b81
Lisbeth Berg <lisbeth@elder.no>
2024-03-14 15:26:40 +0100

    fix: replace fontawesome component with iconify fontawesome

--- src/component.vue
+++ src/component.vue
@@ -43,11 +43,7 @@
             <SlotHandler :value="content.messages.error" :reset="reset" :error="error" />
           </slot>
           <slot name="loader" v-if="isLoading" :content="content">
-            <FontAwesomeIcon
-              class="elder-loader__loading-icon"
-              v-bind="content.icon"
-              spin
-            />
+            <Icon class="elder-loader__loading-icon" v-bind="content.icon" spin />
             <div class="elder-loader__loading-message">
               <slot name="message">{{ content.messages.loading }}</slot>
             </div>
@@ -59,11 +55,10 @@
 </template>
 
 <script>
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Icon from '@kvass/vue2-icon/src/Legacy'
 import { Options } from '../index'
 import { iconBinding, isPromise } from './utils'
 import SlotHandler from './SlotHandler'
-import './icons'
 
 export default {
   props: {
@@ -165,7 +160,7 @@ export default {
     },
   },
   components: {
-    FontAwesomeIcon,
+    Icon,
     SlotHandler,
   },
 }
--- src/icons.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons/faCircleNotch'
-
-library.add(faCircleNotch)
